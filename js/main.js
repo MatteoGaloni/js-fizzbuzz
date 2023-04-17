c385e72d2d0075bd0eff91a90de22f7f79254275
MatteoGaloni <mtgaloni@gmail.com>
2023-04-17 16:42:15 +0200

    add number divided by 3 and 5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 for (let i = 1; i <= 100; i++) {
 
-    console.log(`Contatore vale: ${i}`);
+   
     
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log(`Divisibile sia per 5 che per 3: ${i}`);
+    } else if (i % 5 == 0) {
+        console.log(`Divisibile per 5: ${i}`);
+    } else if (i % 3 == 0) {
+        console.log(`Divisibile per 3: ${i}`);
+    }
+    console.log(`Contatore vale: ${i}`);
 }
+
+
+
+
+
