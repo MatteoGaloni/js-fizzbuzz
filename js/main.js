5a7ea76a3cf6c44b1f7cee7c2ddcfc913eda3843
MatteoGaloni <mtgaloni@gmail.com>
2023-04-17 21:48:19 +0200

    add specific class in order to customize <box>

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-for (let i = 1; i <= 100; i++) {
+const wrapper = document.getElementById("wrapper");
+const box = document.getElementsByClassName("box")
 
-   
+for (let i = 1; i <= 100; i++) {   
     
     if (i % 5 == 0 && i % 3 == 0) {
-        console.log(`Divisibile sia per 5 che per 3: ${i}`);
+        console.log(`FizzBuzz: ${i}`);
+        wrapper.innerHTML += `<div class="box fizzBuzz">${"FizzBuzz"}</div>`;
     } else if (i % 5 == 0) {
-        console.log(`Divisibile per 5: ${i}`);
+        console.log(`Buzz: ${i}`);
+        wrapper.innerHTML += `<div class="box buzz">${"Buzz"}</div>`;
     } else if (i % 3 == 0) {
-        console.log(`Divisibile per 3: ${i}`);
+        console.log(`Fizz: ${i}`);
+        wrapper.innerHTML += `<div class="box fizz">${"Fizz"}</div>`;
+    } else {
+        console.log(i);
+        wrapper.innerHTML += `<div class="box">${i}</div>`;
     }
-    console.log(`Contatore vale: ${i}`);
+
 }
+
 
 
 
